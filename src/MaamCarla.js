1db1cda89805bf457f54f792caea4240d664d2c2
ishmael cascabel <cascabel_ishmael@yahoo.com>
2023-10-06 09:25:36 +0800

    Update MaamCarla.js

--- src/MaamCarla.js
+++ src/MaamCarla.js
@@ -37,7 +37,7 @@ function MaamCarla() {
     const message = "Madayaw and Mabuhay! On this occasion of Teachers' Day, we wish to express my profound gratitude to you, Ms. Carla Almazan. Your unwavering dedication, remarkable patience, and your role as an appreciative educator have left an indelible mark on us. Among the faculty at STI Tagum College, you stand as one of our most cherished mentors, having introduced unique transformations to our academic journey. We acknowledge that our collective behavior may at times disappoint or even offend you, given the headstrong and disrespectful tendencies displayed by some of my fellow classmates. However, please always bear in mind, Ma'am Carla, that we remain receptive to your guidance and discussions. Your wisdom is a beacon that we eagerly follow. Please, continue to exemplify the qualities of a virtuous and equitable instructor, for it is these very attributes that endear you to us. Once again, thank you, from the depths of our hearts. 🫶 P.S. You are undeniably stunning, Ma'am, your beauty truly leaves us in awe. Like AS-IN. HAHAHAHAHAHAHA. Take care always, Ma'am. ❤️";
 
     return (
-        <div className="h-full bg-[url('./assets/vintage.jpg')] w-full">
+        <div className="h-ful w-full">
             
             <div className='flex flex-col justify-center items-center'>
                 <img src={maam} alt="" className="rounded-2xl w-[60%] sm:w-[50%] md:w-[40%] lg:w-[30%] xl:w-[20%] m-5"/>
